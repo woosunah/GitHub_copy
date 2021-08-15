81303ca73a15991747b25174c24d664693c08875
woosunah <sunah5011@gmail.com>
2021-08-15 21:43:25 +0900

    tablet mobile responsive

--- main.js
+++ main.js
@@ -7,9 +7,20 @@
     toggleElements();
   });
 
+  window.addEventListener('resize', function () {
+    if (window.innerWidth > 1024) {
+      offElements();
+    }
+  });
+
   function toggleElements() {
     [].forEach.call(toggles, function (toggle) {
       toggle.classList.toggle('on');
+    });
+  }
+  function offElements() {
+    [].forEach.call(toggles, function (toggle) {
+      toggle.classList.remove('on');
     });
   }
 })(window, document);
